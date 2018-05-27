8d0b62207ddd49aaa9b8c3a7b2b471ab46b4db72
thomas jouannic <thomas.jouannic@nomadeducation.fr>
2018-05-27 16:51:16 +0200

    refactor(frontend): add general "browser" rules

--- frontend.js
+++ frontend.js
@@ -1,16 +1,24 @@
 module.exports = {
+    "parserOptions": {
+        "sourceType": "module"
+    },
     // recommended rules:
     // - https://github.com/vuejs/eslint-plugin-vue#priority-c-recommended-minimizing-arbitrary-choices-and-cognitive-overhead
-    "extends": [
-        "plugin:vue/recommended"
-    ],
+    "extends": "plugin:vue/recommended",
     // required to lint *.vue files
-    "plugins": [
-        "vue"
-    ],
+    "plugins": ["vue"],
     "rules": {
+        //
+        // browser
+        //
+        "no-implicit-globals": "error",
+        "no-implied-eval": "error",
         // we must avoid "console" calls in the frontend context
         "no-console": "error",
+        "no-script-url": "error",
+        //
+        // vue.js
+        //
         "vue/html-indent": [
             "error",
             4
